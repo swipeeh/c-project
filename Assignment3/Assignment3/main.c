07265ad85d948454fa5e6609099819c742e16210
Patrik <260026@via.dk>
2018-04-17 10:30:33 +0200

    initial commit

---
+++ Assignment3/Assignment3/main.c
@@ -0,0 +1,18 @@
+/*
+ * Assignment3.c
+ *
+ * Created: 4/17/2018 10:29:35
+ * Author : Patrik
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
