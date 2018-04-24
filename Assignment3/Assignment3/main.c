a84ce8d36b6ce7241afb164af33802aa605a06da
Patrik I <260010@via.dk>
2018-04-24 10:12:52 +0200

    libraries updated

--- Assignment3/Assignment3/main.c
+++ Assignment3/Assignment3/main.c
@@ -6,13 +6,13 @@
  */ 
 
 #include <avr/io.h>
+#include <stdio.h>
+#include <avr/interrupt.h>
+#include "atmega2560_drivers.h"
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	init_stdio(0, 10000000L);
 }
 
