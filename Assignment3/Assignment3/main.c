1038b29517af6f44b77b4a41246dc5de564a893e
Patrik I <260010@via.dk>
2018-04-26 09:20:52 +0200

    update

--- Assignment3/Assignment3/main.c
+++ Assignment3/Assignment3/main.c
@@ -12,8 +12,37 @@
 
 init_stdio(0, 10000000L);
 
+void nextLevel(int level);
+
 int main(void)
 {
-
+	DDRA = 0xff;
+	//int level = 1;
+	
+	for (int i = 1; i < 3; i++)
+	{
+		if()
+		{
+		nextLevel(i);
+		PORTA = 0b11111111;
+		_delay_ms(1000);
+		}
+	}
 }
 
+void nextLevel(int level)
+{
+	
+	if (level == 1)
+	{
+		PORTA = 0b11111101;
+		_delay_ms(1000);
+	}
+	else if(level == 2)
+	{
+		PORTA = 0b11111101;
+		_delay_ms(1000);
+		PORTA = 0b01111111;
+		_delay_ms(1000);
+	}
+}
