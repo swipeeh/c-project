1d3a778cd96f384da01bcfea595ab0060323c097
Zahari Dzhelepov <253925@via.dk>
2018-04-30 11:53:29 +0200

    random sequence and read input from user

--- Assignment3/Assignment3/MemoryGame.c
+++ Assignment3/Assignment3/MemoryGame.c
@@ -9,9 +9,7 @@
 #include <avr/io.h>
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
 #include <util/delay.h>
-=======
 #define F_CPU 10000000L
 #include <avr/io.h>
 #include <stdio.h>
@@ -23,7 +21,6 @@
 #include <util/delay.h>
 #include <time.h>
 
->>>>>>> 52920d2e0cb5ba3205d94a1e4efb8eb5448f5b31
 
 void startGame(){
 		
@@ -42,11 +39,74 @@ void delay()
 	printf("PICKLE RIIIICK");	 
 	_delay_ms(10000);
 	};
-void randomSequence(){
 	
+int randomNumber(){
+	int n = rand() % 7;
+	printf("%d ", n);
+	return n;
+}
+void randomSequence(memorygame_t *self){
+	int *sequence = self -> sequence;
+	int i;
+	for (i= 0; i < (self ->level + 1); i++)
+	{
+		sequence[i] = randomNumber();
+	}
 	};
+bool compareNumbers(int a,int b){
+	if(a == b){
+		return true;
+	}
+	return false;
+	};
+int convertValuefromPINBtoInt(int value){
+	if(value == 0){
+		PORTA = 254;
+		_delay_ms(1000);
+	}else if (value == 1)
+	{
+		PORTA = 253;
+		_delay_ms(1000);
+	}
+	else if (value == 2)
+	{
+		PORTA = 251;
+		_delay_ms(1000);
+	}
+	else if (value == 3)
+	{
+		PORTA = 247;
+		_delay_ms(1000);
+	}
+	else if (value == 4)
+	{
+		PORTA = 239;
+		_delay_ms(1000);
+	}
+	else if (value == 5)
+	{
+		PORTA = 223;
+		_delay_ms(1000);
+	}else if (value == 6)
+	{
+		PORTA = 191;
+		_delay_ms(1000);
+	}else if (value == 7)
+	{
+		PORTA = 127;
+		_delay_ms(1000);
+	}
+	else{
+		return -1;
+	}
+}
 void readUserInput(){
-	
+	int n = PINB;
+	while(PINB == 0xFF){
+		n = convertValuefromPINBtoInt(n);
+		printf("PINB %d", n);
+		return n;
+	}
 	};
 void randomLevels(){
 	
